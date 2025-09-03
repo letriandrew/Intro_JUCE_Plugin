ba6c7e8d29cd6df39652c4bb1226c1d18c5c4014
Andrew Le <letriandrew@gmail.com>
2025-09-02 21:15:55 -0700

    cmake init

---
+++ build/CPM_modules/FindJUCE.cmake
@@ -0,0 +1,3 @@
+include("/Users/ale/Documents/Programming/Intro JUCE Plugin/build/cmake/CPM.cmake")
+CPMAddPackage("NAME;JUCE;GITHUB_REPOSITORY;juce-framework/JUCE;GIT_TAG;8.0.9;VERSION;8.0.9")
+set(JUCE_FOUND TRUE)
